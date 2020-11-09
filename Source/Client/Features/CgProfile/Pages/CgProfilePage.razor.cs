2bc9dbc2e0c65f90ab045077ca606915a21dc08b
Supakorn Yoshino <yoshinotsubagi@gmail.com>
2020-11-09 11:28:29 +0700

    add razor cs

---
+++ Source/Client/Features/CgProfile/Pages/CgProfilePage.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace MyBlazorApp.Pages
+{
+  using System.Threading.Tasks;
+  using MyBlazorApp.Features.Bases;
+  using static MyBlazorApp.Features.Profiles.CgProfileState;
+  public partial class CgProfilePage : BaseComponent
+  {
+    private const string RouteTemplate = "/CgProfile";
+
+    public static string GetRoute() => RouteTemplate;
+
+    protected override async Task OnInitializedAsync() =>
+      await Send(new FetchCgProfileAction()).ConfigureAwait(false);
+  }
+}
